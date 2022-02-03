90171d40883c19830ce5c868148a8019dbe336d9
Nicolas Palumbo <nicolas.palumbo@loveholidays.com>
2022-02-03 23:20:31 +0100

    another attempt to try to use fix for paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-for md in $(find . -name "*.md"); do /render-md-mermaid.sh "$(md| sed 's/ /\\\ /g')" in-container; done
+for md in $(find . -name "*.md"|sed 's/ /\\\ /g'); do /render-md-mermaid.sh "$md" in-container; done
